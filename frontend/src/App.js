b76533a4ccc3ad47f72615ab44ac34f41bed9957
anast404 <anasty2608@yandex.ru>
2025-07-03 16:07:16 +0300

    редизайн + три поля
    разделение на поля: тема, предмет, уровень

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,45 @@
-import { useForm, SubmitHandler } from "react-hook-form"
-import './App.css'
+import { useForm, SubmitHandler } from 'react-hook-form';
+import './App.css';
 
 export default function App() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = useForm();
 
-  const onSubmit = (data) => {
-    //ss
-  }
+  const onSubmit = (data) => {};
 
   return (
-    
-      <div align="center" class="App-header">
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h2 align="center" class="text">Конспекты по темам и уровню изучения</h2>
-      <input {...register("conspect", { required: true })} placeholder="Предмет, тема, уровень погружения"/>
-      <button type="submit">Отправить</button>
-    </form>
-      </div>
-    
-  )
-  
+    <div align="center">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div class="design1"></div>
+        <h1 align="center" class="text">
+          Создать конспект
+        </h1>
+
+        <div>
+          <input
+            {...register('conspect', { required: true })}
+            placeholder="Предмет"
+            style={{ fontSize: '14px' }}
+          />
+          <input
+            {...register('theme', { required: true })}
+            placeholder="Тема"
+            style={{ fontSize: '14px' }}
+          />
+          <input
+            {...register('level', { required: true })}
+            placeholder="Уровень погружения"
+            style={{ fontSize: '14px' }}
+          />
+        </div>
+        <button type="submit" class="b2">
+          Отправить
+        </button>
+        <div class="design2"></div>
+      </form>
+    </div>
+  );
 }
