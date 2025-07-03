8cbe974db3adc36eee67542382f6a25aba032d7f
anast404 <anasty2608@yandex.ru>
2025-07-03 16:41:04 +0300

    +router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,34 @@
-import { useForm, SubmitHandler } from 'react-hook-form';
-import './App.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Outlet,
+} from 'react-router';
+import CreateConspects from './pages/CreateConspects/CreateConspects';
 
-export default function App() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = (data) => {};
-
+const Root = () => {
   return (
-    <div align="center">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div class="design1"></div>
-        <h1 align="center" class="text">
-          Создать конспект
-        </h1>
-
-        <div>
-          <input
-            {...register('conspect', { required: true })}
-            placeholder="Предмет"
-            style={{ fontSize: '14px' }}
-          />
-          <input
-            {...register('theme', { required: true })}
-            placeholder="Тема"
-            style={{ fontSize: '14px' }}
-          />
-          <input
-            {...register('level', { required: true })}
-            placeholder="Уровень погружения"
-            style={{ fontSize: '14px' }}
-          />
-        </div>
-        <button type="submit" class="b2">
-          Отправить
-        </button>
-        <div class="design2"></div>
-      </form>
+    <div>
+      <Outlet />
     </div>
   );
-}
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<CreateConspects />} />
+    </Route>,
+  ),
+);
+
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/CreateConspects/CreateConspects.jsx
@@ -0,0 +1,47 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+import '../../App.css';
+
+function CreateConspects() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {};
+
+  return (
+    <div align="center">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div class="design1"></div>
+        <h1 align="center" class="text">
+          Создать конспект
+        </h1>
+
+        <div>
+          <input
+            {...register('conspect', { required: true })}
+            placeholder="Предмет"
+            style={{ fontSize: '14px' }}
+          />
+          <input
+            {...register('theme', { required: true })}
+            placeholder="Тема"
+            style={{ fontSize: '14px' }}
+          />
+          <input
+            {...register('level', { required: true })}
+            placeholder="Уровень погружения"
+            style={{ fontSize: '14px' }}
+          />
+        </div>
+        <button type="submit" class="b2">
+          Отправить
+        </button>
+        <div class="design2"></div>
+      </form>
+    </div>
+  );
+}
+
+export default CreateConspects;
